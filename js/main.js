51381a573032b7ead399e044859924cffa4e09bc
Max Zhao <interarticle@gmail.com>
2015-03-13 19:06:29 -0400

    Huh.

--- js/main.js
+++ js/main.js
@@ -65,10 +65,9 @@ user_management_system.config(['$routeProvider',
 
 user_management_system.run(['$rootScope', '$location', 'capi.ums',
     function($rootScope, $location, ums) {
-        $rootScope.$on('$locationChangeStart', function(event, next) {
+        $rootScope.$on('$routeChangeStart', function(event, next) {
             if (next.requiresLogin && !ums.is_logged_in()) {
                 console.log(next);
-                console.log($location.path());
                 console.log("DENIED");
                 event.preventDefault();
                 $rootScope.$evalAsync(function() {
